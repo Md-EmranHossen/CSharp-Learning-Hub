26229810ca48582595adab355e3e0b287c635ae5
EmranHossen <emranhossen3075@gmail.com>
2025-04-27 22:05:20 +0600

    Class 23

---
+++ LearnCSharp/Indexers/Program.cs
@@ -0,0 +1,15 @@
+﻿// Array
+using Indexers;
+
+int[] ages = [22, 25];
+ages[0] = 21;
+
+// List ( List a normal class)
+List<string> names = new List<string> { "Emran Hossen", "Rakib Hossain" };
+names[0] = "Narmi";
+
+// How to access our custom class like List (using indexers)
+ShoppingCart cart = new ShoppingCart();
+cart[0] = new Product();
+cart["4"] = new Product();
+cart[3, 4.5] = new Product();
---
+++ LearnCSharp/Indexers/ShoppingCart.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexers
+{
+    public class ShoppingCart
+    {
+        private List<Product> products = new List<Product>();
+        public Product this[int position]
+        {
+            get
+            {
+                return products[position];
+            }
+            set
+            {
+                products[position] = value;
+            }
+        }
+
+
+        // Access using string
+        public Product this[string position]
+        {
+            get
+            {
+                return products[int.Parse(position)];
+            }
+            set
+            {
+                products[int.Parse(position)] = value;
+            }
+        }
+
+
+        // Multipule parameters
+        public Product this[int x, double y]
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+
+            }
+        }
+    }
+}

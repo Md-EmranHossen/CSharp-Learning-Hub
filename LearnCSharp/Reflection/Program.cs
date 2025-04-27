a1646e203a71008e610acd67cb6e00b05b537b09
EmranHossen <emranhossen3075@gmail.com>
2025-04-27 21:27:02 +0600

    class 24

---
+++ LearnCSharp/Reflection/Program.cs
@@ -0,0 +1,39 @@
+﻿/*
+string name = "Emran";
+Type t2 = name.GetType();
+*/
+
+
+using System.Reflection;
+
+Type t = typeof(Math);
+
+MethodInfo[] methods = t.GetMethods();
+int x = 0;
+foreach (var method in methods)
+{
+    ParameterInfo[] parameters = method.GetParameters();
+    Type returntype = method.ReturnType;
+
+    Console.Write(returntype.Name.ToLower());
+    Console.Write(" ");
+    Console.WriteLine(method.Name);
+
+    Console.Write("(");
+    if(parameters.Length > 0)
+    {
+        foreach(var parameter in parameters)
+        {
+            Console.Write(parameter.ParameterType.Name);
+            Console.Write(' ');
+            Console.Write(parameter.Name);
+
+            if (parameters[parameters.Length - 1].Name != parameter.Name)
+            {
+                Console.Write(",");
+            }
+        }
+    }
+    Console.WriteLine(")");
+    Console.WriteLine();
+}

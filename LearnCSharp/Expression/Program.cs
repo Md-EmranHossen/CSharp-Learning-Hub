c206b8386f1f8a51bd1ca42228ddae49beba15e6
EmranHossen <emranhossen3075@gmail.com>
2025-04-27 21:03:09 +0600

    Class 24

---
+++ LearnCSharp/Expression/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq.Expressions;
+
+Func<int, int, double> div = (a, b) => a / b*1.0;
+double r = div(3, 4);
+
+Console.WriteLine(r);
+
+//Expression  
+Expression<Func<int,int,double>> e = (a, b) => a / b*1.0;
+var f = e.Compile();
+f(5, 2);
+Console.WriteLine(f); 

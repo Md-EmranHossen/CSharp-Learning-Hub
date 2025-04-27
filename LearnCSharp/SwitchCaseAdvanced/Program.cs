ce9b702386ed7a7a86be4d3a66bd19bbbe8b2bfd
EmranHossen <emranhossen3075@gmail.com>
2025-04-27 14:01:56 +0600

    Class 24

---
+++ LearnCSharp/SwitchCaseAdvanced/Program.cs
@@ -0,0 +1,51 @@
+﻿// Basic Switch Case (Previous Syntax) 
+using SwitchCaseAdvanced;
+
+var option = 5;
+int x = 0;
+
+switch (option) // Option Must be primitive type.
+{
+    case 0:
+        x += 5;
+        break;
+    case 1:
+        x = 5;
+        break;
+    case 2:
+        x += 3;
+        break;
+    case 3:
+        x = 0;
+        break;
+    case 4:
+        x += 4;
+        break;
+    case 5:
+        x -= 5;
+        break;
+    default:
+        x = 1;
+        break;
+}
+
+
+// New syntax of Switch Case (From .NET 5)
+x = option switch
+{
+    0 => x + 5,
+    1 => 5,
+    2 => 5 / 2,
+    _ => 1
+};
+
+
+// Option become object Type
+var person = new Person { Name = "Emran", Age = 42 };
+
+x = person switch
+{
+    { Age: 40, Name: "Sajib" } => 5,
+    { Age: 25, Name: "Rakib" } => 8,
+    { Age: _, Name: _ } => 0
+};

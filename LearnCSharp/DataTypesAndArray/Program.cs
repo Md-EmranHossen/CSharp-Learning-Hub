5492f45fc6f384cdf85f6b09f4ef804c379fa740
EmranHossen <emranhossen3075@gmail.com>
2025-05-09 08:38:42 +0600

    Class 3

---
+++ LearnCSharp/DataTypesAndArray/Program.cs
@@ -0,0 +1,27 @@
+﻿int[] Intake = new int[4];
+
+Intake[1] = 4;
+Intake[2] = 6;
+Intake[3] = 7;
+
+int[] Section = [3, 4, 5, 6, 4, 3,];
+
+foreach (int student in Intake)
+{
+    Console.WriteLine(student);
+}
+
+// 2D Array
+int[,] grid = new int[2, 3];
+
+grid[0, 0] = 1;
+grid[1, 0] = 4;
+
+for (int i = 0; i < 2; i++)
+{
+    for (int j = 0; j < 3; j++)
+    {
+        Console.Write(grid[i, j] + " ");
+    }
+    Console.WriteLine();
+}

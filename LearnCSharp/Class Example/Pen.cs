294cb04f2de49cc7204042b800a0c9ae08081812
EmranHossen <emranhossen3075@gmail.com>
2025-05-10 16:47:26 +0600

    Class 06

---
+++ LearnCSharp/Class Example/Pen.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Example
+{
+    public class Pen
+    {
+        private string _inkColor;
+        private string _model;
+        private int _inkAmount;
+        public string Somthing { get; set; }
+
+        public int InkAmount
+        {
+            get
+            {
+                if(_inkAmount < 0)
+                {
+                    _inkAmount = 0;
+                }
+                return _inkAmount;
+            }
+            set
+            {
+                InkAmount = value;
+            }
+        }
+
+        public Pen(string inkColour, string model, int inkAmount)
+        {
+            _inkAmount = inkAmount;
+            _model = model;
+            _inkColor = inkColour;
+        }
+
+
+
+        public void Write(string text)
+        {
+            if (_inkAmount >= text.Length)
+            {
+                Console.WriteLine(text);
+                _inkAmount -= text.Length;
+            }
+        }
+    }
+}
---
+++ LearnCSharp/Class Example/Program.cs
@@ -0,0 +1,11 @@
+﻿using Class_Example;
+
+Pen redPen = new Pen("red", "smart", 100);
+Pen bluePen = new Pen("blue", "smart", 200);
+
+bluePen.Write("Hello world");
+redPen.Write("Hello world Again");
+
+Console.WriteLine(redPen.InkAmount);
+
+redPen.InkAmount = 9;

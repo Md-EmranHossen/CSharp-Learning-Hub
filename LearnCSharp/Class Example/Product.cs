f7df1b496ca424f0341526c2dc3739216eb8d654
EmranHossen <emranhossen3075@gmail.com>
2025-05-10 21:55:10 +0600

    Class 7

---
+++ LearnCSharp/Class Example/Product.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Devskill.CharpExamples.ClassExamples
+{
+    internal class Product
+    {
+        private readonly double _defaultDiscount = 10; // Only chagne in constructor
+        private const double PI = 3.1416;  // Never change
+        public double Price { get; set; }
+
+        public Product() : this(0)
+        {
+
+        }
+
+        public Product(double price) : this(price, 10)
+        {
+
+        }
+
+        public Product(double price, double defaultDiscount)
+        {
+            Price = price;
+            _defaultDiscount = defaultDiscount;
+        }
+
+
+        ~Product()
+        {
+
+        }
+
+        public double GetDiscount()
+        {
+            return Price * _defaultDiscount / 100;
+        }
+
+        public double GetDiscount(double discount)
+        {
+            return Price * discount / 100;
+        }
+
+        public double GetDiscount(double maxdiscount, double discountAmount)
+        {
+            double discount = Price * discountAmount / 100;
+            if (discount > maxdiscount)
+            {
+                return maxdiscount;
+            }
+            else
+                return discount;
+        }
+
+        public double GetDiscount(int maxdiscount, double discountAmount)
+        {
+            double discount = Price * discountAmount / 100;
+            if (discount > maxdiscount)
+            {
+                return maxdiscount;
+            }
+            else
+                return discount;
+        }
+        public double GetDiscount(double discountAmount, int maxdiscount)
+        {
+            return GetDiscount(maxdiscount, discountAmount);
+        }
+    }
+
+}
--- LearnCSharp/Class Example/Program.cs
+++ LearnCSharp/Class Example/Program.cs
@@ -1,5 +1,5 @@
-﻿using Class_Example;
-
+﻿using Devskill.CharpExamples.ClassExamples;
+using Class_Example;
 Pen redPen = new Pen("red", "smart", 100);
 Pen bluePen = new Pen("blue", "smart", 200);
 
@@ -9,3 +9,10 @@ redPen.Write("Hello world Again");
 Console.WriteLine(redPen.InkAmount);
 
 redPen.InkAmount = 9;
+
+Product product1 = new Product();
+Product product2 = new Product();
+
+
+product1.Price = 2000;
+double discount = product1.GetDiscount();
